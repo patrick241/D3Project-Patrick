0278fccb4fb9e8032b04a8a498621abbd24463cd
shado <shado@DESKTOP-72F9AGT>
2022-06-05 21:46:12 -0400

    1.0

---
+++ D3Project-Patrick/src/Main.java
@@ -0,0 +1,10 @@
+
+import java.awt.*;
+
+class Main {
+
+  public static void main(String[] args) {
+    Calculator one = new Calculator(new Color(255, 75, 75, 80),"Courier");
+    Calculator two = new Calculator(new Color(75, 125, 255, 80),"Lucida Sans Unicode");
+  }
+}
